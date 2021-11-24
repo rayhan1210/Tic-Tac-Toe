25979092543da0a157f6aa5f087b6a227919ab36
Rayhan <rayannnn1210@gmail.com>
2021-11-24 19:27:19 +1100

    fixed run time error

--- gameplay.py
+++ gameplay.py
@@ -2,51 +2,86 @@ import pygame
 import sys
 import structure
 
+board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
 
-def start_game(screen, mid_area, mid_left, mid_right, top_area, top_right, top_left, btm_area, btm_right, btm_left):
-    print("in start_game")
-    while True:
+
+# mid_area = board[1][1]
+# mid_left = board[1][0]
+# mid_right = board[1][2]
+
+def start_game(screen, FPS, mid_area, mid_left, mid_right, top_area, top_right, top_left, btm_area, btm_right,
+               btm_left):
+    # checkBoard(board)
+    player = 0
+    value = True
+    clock = pygame.time.Clock()
+    while value:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                # self.loop = False
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
+                player = changePlayer(player)
                 if mid_area.collidepoint(event.pos):  # if mouse is inside the area use collide point to check that
                     print("in mid")
-                    # structure.draw_gamechar_x(screen, 0, 120)
-                    structure.draw_circle(screen, 0, 120)
+                    game_char(player, screen, 0, 120, 1, 1, board)
+                    print(board[1][1])
                 elif mid_left.collidepoint(event.pos):
                     print("In mid left")
-                    # structure.draw_gamechar_x(screen, -135, 120)
-                    structure.draw_circle(screen, -130, 120)
+                    game_char(player, screen, -135, 120)
                 elif mid_right.collidepoint(event.pos):
                     print("in mid right")
-                    # structure.draw_gamechar_x(screen, 135, 120)
-                    structure.draw_circle(screen, 130, 120)
+                    game_char(player, screen, 135, 120)
                 elif top_area.collidepoint(event.pos):
                     print("In top ")
-                    # structure.draw_gamechar_x(screen, 0, 0)
-                    structure.draw_circle(screen, 0, 0)
+                    game_char(player, screen, 0, 0)
                 elif top_left.collidepoint(event.pos):
                     print("in top left")
-                    # structure.draw_gamechar_x(screen, -135, 0)
-                    structure.draw_circle(screen, -130, 0)
+                    game_char(player, screen, -135, 0)
                 elif top_right.collidepoint(event.pos):
                     print("In top right")
-                    # structure.draw_gamechar_x(screen, 135, 0)
-                    structure.draw_circle(screen, 130, 0)
+                    game_char(player, screen, 135, 0)
                 elif btm_area.collidepoint(event.pos):
                     print("In btm")
-                    # structure.draw_gamechar_x(screen, 0, 240)
-                    structure.draw_circle(screen, 0, 240)
+                    game_char(player, screen, 0, 240)
                 elif btm_right.collidepoint(event.pos):
                     print("in btm right")
-                    # structure.draw_gamechar_x(screen, 135, 240)
-                    structure.draw_circle(screen, 130, 240)
+                    game_char(player, screen, 135, 240)
                 elif btm_left.collidepoint(event.pos):
                     print("In btm left")
-                    # structure.GameStructure().draw_gamechar_x(screen, -135, 240)
-                    structure.draw_circle(screen, -130, 240)
+                    game_char(player, screen, -135, 240)
                 else:
                     print("error")
-        pygame.display.flip()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    structure.main_menu(screen)
+                    value = False
+        pygame.display.update()
+
+
+def checkBoard(board, i, j):
+    print(f"board: {board[i][j]}")
+
+
+def game_char(player, screen, x, y, i, j, b):
+    if player == 1:
+        structure.draw_cross(screen, x, y)
+        b[i][j] = 1
+        # print(b[i][j])
+    if player == 2:
+        structure.draw_circle(screen, x, y)
+        b[i][j] = 2
+
+
+def changePlayer(player):
+    if player == 0:
+        player = 1
+    elif player == 1:
+        player = 2
+    else:
+        player = 1
+    return player
--- structure.py
+++ structure.py
@@ -31,54 +31,54 @@ radius = 50
 draw_o = [
     [285, 120]
 ]
+FPS = 60
+rect_area_setting = pygame.Rect(210, 250, 170, 40)
+rect_area_game = pygame.Rect(210, 200, 170, 40)
 
 
 def display_window(screen):
-    click = False
-    while True:
-        mx, my = pygame.mouse.get_pos()
-        rect_area_setting = pygame.Rect(210, 250, 170, 40)
-        rect_area_game = pygame.Rect(210, 200, 170, 40)
-        main_menu(screen)
-        if rect_area_game.collidepoint((mx, my)):
-            if click:
-                draw_game_outlier(screen, red, points)
-                gameplay.start_game(screen, mid_area, mid_left, mid_right, top_area, top_right, top_left,
-                                    btm_area, btm_right, btm_left)
+    value = True
+    clock = pygame.time.Clock()
+    main_menu(screen)
+    while value:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
+                value = False
             if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    click = True
+                if rect_area_game.collidepoint(event.pos):
+                    draw_game_outlier(screen, red, points)
+                    gameplay.start_game(screen, FPS, mid_area, mid_left, mid_right, top_area, top_right, top_left,
+                                        btm_area, btm_right, btm_left)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    value = False
+        # main_menu(screen)
+        # print(f"fps: {clock.get_fps()}")
         pygame.display.update()
 
 
 def main_menu(screen):
-    pygame.font.init()
+    screen.fill((0, 0, 0))
+    # pygame.font.init()
     game_font = pygame.font.SysFont("Comic Sans MS", 30)
     text_surface = game_font.render("Main Menu", False, red)
     screen.blit(text_surface, (220, 120))  # draws image on to another, in this case text on the screen
-    one_player(screen)
-    two_player(screen)
+    menu_box(screen, "Comic Sans MS", "Player One", 210, 200, 170, 40, 222, 195)
+    menu_box(screen, "Comic Sans MS", "Settings", 210, 250, 170, 40, 240, 245)
+    pygame.display.update()
 
 
-def one_player(screen):
-    pygame.draw.rect(screen, red, (210, 200, 170, 40), width=3)
-    game_font = pygame.font.SysFont("Comic Sans MS", 30)
-    text_surface = game_font.render("One Player", False, red)
-    screen.blit(text_surface, (222, 195))
-
-
-def two_player(screen):
-    pygame.draw.rect(screen, red, (210, 250, 170, 40), width=3)
-    game_font = pygame.font.SysFont("Comic Sans MS", 30)
-    text_surface = game_font.render("Settings", False, red)
-    screen.blit(text_surface, (240, 245))
+def menu_box(screen, font, text, x, y, width, height, a, b):
+    pygame.draw.rect(screen, red, (x, y, width, height), width=3)
+    game_font = pygame.font.SysFont(font, 30)
+    text_surface = game_font.render(text, False, red)
+    screen.blit(text_surface, (a, b))
 
 
 def draw_game_outlier(screen, color, coordinates):
     screen.fill((0, 0, 0))
+    # print("screen")
     pygame.draw.line(screen, color, coordinates[0], coordinates[1], width=3)
     pygame.draw.line(screen, color, coordinates[2], coordinates[3], width=3)
     pygame.draw.line(screen, color, coordinates[4], coordinates[5], width=3)
@@ -86,7 +86,7 @@ def draw_game_outlier(screen, color, coordinates):
     pygame.display.update()
 
 
-def draw_gamechar_x(screen, x, y):
+def draw_cross(screen, x, y):
     pygame.draw.line(screen, red, (draw_x[0][0] + x, draw_x[0][1] + y),
                      (draw_x[1][0] + x, draw_x[1][1] + y), width=3)
     pygame.draw.line(screen, red, (draw_x[2][0] + x, draw_x[2][1] + y),
